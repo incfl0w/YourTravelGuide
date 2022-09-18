d681bd1cc1bab0e3fabf35f533e659ee5dcd7cf4
Dima <dmitriiburiachek@gmail.com>
2022-09-18 17:02:24 +0300

    minor changes

--- ytg/accounts/api/urls.py
+++ ytg/accounts/api/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path, include
 from . import views
 
-path('login/', include('rest_framework.urls')), 
-path('signup/', views.signup)
+
+urlpatterns = [
+    path('login/', include('rest_framework.urls')), 
+    path('signup/', views.signup)
+]
+
