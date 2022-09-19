16f1303c312dd975db989eb7029645ee9f40d512
Dima <dmitriiburiachek@gmail.com>
2022-09-19 13:03:30 +0300

    serializers for countries and cities

--- ytg/countries/api/urls.py
+++ ytg/countries/api/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
-from .views import PostRetrieveUpdateDestroy, PostList, VoteCreate
-
+from .views import PostRetrieveUpdateDestroy, PostList, VoteCreate, CountryList, CityList
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
+    #post
     path('posts/', PostList.as_view()),
     path('posts/<int:pk>', PostRetrieveUpdateDestroy.as_view()), 
     path('posts/<int:pk>/vote', VoteCreate.as_view()),
+    #city
+    path('cities/', CityList.as_view()),
+    
+    #country
+    path('countries/', CountryList.as_view()),
+    
     
 ]
+
